b45ca3d6922c70f43ec755a514ef6d6af84f0d4d
Arne Kuederle <a.kuederle@gmail.com>
2015-01-06 10:30:20 +0100

    FIxed bugs occurring while testing with the chopper at 5/1/15.
    Added missing baudrate parameter.
    Added a empty command send in the beginning (necessary to establish the connection)
    Changed all Range checks to use the namedtuple to access the range.
    Adjusted read and cut length for each command
    Added a read after each set command to clean the read buffer for the following get command.
    Fixed minor typos
    Changed the get_all function to exclude the all command from the Get list to prevent an endless loop

--- chopper_controller.py
+++ chopper_controller.py
@@ -20,10 +20,11 @@ class CHOPPER(object):
     _bladerange = namedtuple("bladerange", _blades)
 
 
-    def __init__(self, port=0, log=False):
+    def __init__(self, port=3, log=False):
         import serial
         super(CHOPPER, self).__init__()
         self.ser = serial.Serial(port)
+        self.ser.baudrate = 115200
         self.ser.timeout = 1
         self.ser.bytesize = serial.EIGHTBITS
         self.ser.parity = serial.PARITY_NONE
@@ -31,6 +32,7 @@ class CHOPPER(object):
         self.ser.xonxoff = 0
         self.ser.rtscts = 0
         self.ser.dsrdtr = 0
+        self.ser.write("\r")
         self.ser.read(100)
         self.log = log
         self.log_file = []
@@ -39,7 +41,7 @@ class CHOPPER(object):
         self.Set = self._control(intfreq=self.set_intfreq, blade=self.set_blade, ref=self.set_ref)
         self.Get = self._query(status=self.get_status, intfreq=self.get_intfreq, exfreq=self.get_exfreq, blade=self.get_blade, ref=self.get_ref, all=self.get_all)
         self.Stat = self._state(status=None, intfreq=None, exfreq=None, blade=None, ref=None)
-        self.get_all()
+        self.Get.all()
 
     def _log_write(self, string):
         if self.log is True:
@@ -52,19 +54,20 @@ class CHOPPER(object):
         command = "freq?\r"
         self._log_write(command)
         self.ser.write(command)
-        answer = self.ser.read(50)  # adjust!
+        answer = self.ser.read(15)  # adjust!
         self._log_write(answer)
-        rlvalue = float(answer)
+        rlvalue = float(answer[len(command):-3])
         self.Stat = self.Stat._replace(intfreq=rlvalue)
         return rlvalue
 
     def set_intfreq(self, value):
         "set the internal frequency"
-        if float(value) < self._intfreq_range[0] or float(value) > self._intfreq_range[1]:
+        if float(value) < self._Range.intfreq[0] or float(value) > self._Range.intfreq[1]:
             raise RangeError("{} is out of range!".format(str(value)))
         command = "freq={}\r".format(str(value))
         self._log_write(command)
         self.ser.write(command)
+        self.ser.read(15)
         rlvalue = self.get_intfreq()
         return rlvalue
 
@@ -73,20 +76,21 @@ class CHOPPER(object):
         command = "blade?\r"
         self._log_write(command)
         self.ser.write(command)
-        answer = self.ser.read(50)  # adjust!
+        answer = self.ser.read(15)  # adjust!
         self._log_write(answer)
-        rlvalue = float(answer)
+        rlvalue = float(answer[len(command):-3])
         self.Stat = self.Stat._replace(blade=rlvalue)
         self._Range = self._Range._replace(intfreq=self._Bladerange[int(rlvalue)])
         return rlvalue
 
     def set_blade(self, value, ):
         "set the blade type"
-        if float(value) < self._blade_range[0] or float(value) > self._blade_range[1]:
+        if float(value) < self._Range.blade[0] or float(value) > self._Range.blade[1]:
             raise RangeError("{} is out of range!".format(str(value)))
         command = "blade={}\r".format(str(int(value)))
         self._log_write(command)
         self.ser.write(command)
+        self.ser.read(15)
         rlvalue = self.get_intfreq()
         return rlvalue
 
@@ -95,20 +99,21 @@ class CHOPPER(object):
         command = "ref?\r"
         self._log_write(command)
         self.ser.write(command)
-        answer = self.ser.read(50)  # adjust!
+        answer = self.ser.read(15)  # adjust!
         self._log_write(answer)
-        rlvalue = float(answer)
+        rlvalue = float(answer[len(command):-3])
         self.Stat = self.Stat._replace(ref=rlvalue)
         return rlvalue
 
     def set_ref(self, value):
-        if float(value) < self._ref_range[0] or float(value) > self._ref_range[1]:
+        if float(value) < self._Range.ref[0] or float(value) > self._Range.ref[1]:
             raise RangeError("{} is out of range!".format(str(value)))
         "set the reference mode"
         command = "ref={}\r".format(str(int(value)))
         self._log_write(command)
         self.ser.write(command)
-        rlvalue = self.get_intfreq()
+        self.ser.read(15)
+        rlvalue = self.get_ref()
         return rlvalue
 
     def get_status(self):
@@ -116,8 +121,8 @@ class CHOPPER(object):
         command = "enable?\r"
         self._log_write(command)
         self.ser.write(command)
-        answer = self.ser.read(50)
-        rlvalue = float(answer)
+        answer = self.ser.read(15)
+        rlvalue = float(answer[len(command):-3])
         self.Stat = self.Stat._replace(status=rlvalue)
         return rlvalue
 
@@ -126,29 +131,26 @@ class CHOPPER(object):
         command = "input?\r"
         self._log_write(command)
         self.ser.write(command)
-        answer = self.ser.read(50)
-        rlvalue = float(answer)
+        answer = self.ser.read(15)
+        rlvalue = float(answer[len(command):-3])
         self.Stat = self.Stat._replace(exfreq=rlvalue)
         return rlvalue
 
     def start(self):
         "send start signal"
-        command = "enable = 1\r"
+        command = "enable=1\r"
         self._log_write(command)
         self.ser.write(command)
-        rlvalue = self.get_status()
-        return rlvalue
 
     def stop(self):
         "send stop signal"
-        command = "enable = 0\r"
+        command = "enable=0\r"
         self._log_write(command)
         self.ser.write(command)
-        rlvalue = self.get_status()
-        return rlvalue
 
     def get_all(self):
-        for name, command in self.Get._asdict().iteritems():
+        for command in self.Get[:-1]:
+            print command
             command()
         return self.Stat
 
